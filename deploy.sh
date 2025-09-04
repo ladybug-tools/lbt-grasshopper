055a47de32e9dd9add0e7b56ba86f3f5bb1097c5
Chris Mackey <chris@ladybug.tools>
2025-09-04 11:56:31 -0700

    ci(release): Upgrade all dependencies used for release

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Building distribution"
-python setup.py sdist bdist_wheel
+python -m build
 echo "Pushing new version to PyPi"
 twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
